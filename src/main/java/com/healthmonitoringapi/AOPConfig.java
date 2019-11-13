d2d003fece9a5076d1995c8098772e42763d09dd
emersonbnp <emersonbnp@gmail.com>
2019-11-12 23:40:30 -0300

    Benchmarking repository method calls

---
+++ src/main/java/com/healthmonitoringapi/AOPConfig.java
@@ -0,0 +1,24 @@
+package com.healthmonitoringapi;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@Aspect
+public class AOPConfig {
+
+	@Around("execution(* com.healthmonitoringapi.repository.*.*(..))")
+	public Object profile(ProceedingJoinPoint pjp) throws Throwable {
+		long start = System.currentTimeMillis();
+		Object output = pjp.proceed();
+		long elapsedTime = System.currentTimeMillis() - start;
+		System.out.println("Method execution time: " + elapsedTime + " milliseconds.");
+		return output;
+	}
+
+}
